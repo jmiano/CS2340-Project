b2980fb9994ed8bf98b023a1391f5efb97274ebf
zfan71 <793318148@qq.com>
2018-09-23 15:51:32 -0400

    second change

--- BuzzTracker/app/src/main/java/com/cs2340/binarybros/buzztracker/PrettyLogin.java
+++ BuzzTracker/app/src/main/java/com/cs2340/binarybros/buzztracker/PrettyLogin.java
@@ -6,12 +6,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.RelativeLayout;
 
 public class PrettyLogin extends AppCompatActivity {
 
     RelativeLayout relay1;
     Handler handler = new Handler();
+    private EditText userID;
+    private EditText password;
+    private TextView loginInfor;
     Runnable runnable = new Runnable() {
         @Override
         public void run() {
@@ -34,7 +39,29 @@ public class PrettyLogin extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(PrettyLogin.this, WelcomeActivity.class));
+//                trial = 4;
+            }
+        });
+        EditText userID = (EditText) findViewById(R.id.Username);
+        EditText password = (EditText) findViewById(R.id.Password);
+        TextView loginInfor = (TextView) findViewById(R.id.logininfor);
+        loginInfor.setText("please type username and password");
+        Button loginBtn = (Button) findViewById(R.id.login_btn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checklogin (userID.getText().toString(),password.getText().toString());
             }
         });
     }
+    private void checklogin(String userid, String password) {
+        if (userid.equals("smartguy")&& password.equals("123456") ){
+            Intent intent = new Intent(PrettyLogin.this, LogOut.class);
+        } else {
+            loginInfor.setText("username or password incorrect");
+        }
+    }
+
 }
+
+
