8279aa3ccd7c2fa7f6a55de4a52b5646c31d681f
Fidel Flores Caceres <fefcaceres@gmail.com>
2018-09-23 11:58:56 -0400

    connect logout button

--- BuzzTracker/app/src/main/java/com/cs2340/binarybros/buzztracker/LogOut.java
+++ BuzzTracker/app/src/main/java/com/cs2340/binarybros/buzztracker/LogOut.java
@@ -1,7 +1,10 @@
 package com.cs2340.binarybros.buzztracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogOut extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class LogOut extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_out);
+
+        Button logoutBtn = (Button) findViewById(R.id.logout_btn);
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LogOut.this, PrettyLogin.class));
+            }
+        });
+
     }
+
+
+
+
+
+
 }
