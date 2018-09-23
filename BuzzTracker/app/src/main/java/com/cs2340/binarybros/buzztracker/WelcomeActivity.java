978216b1c30812227c07eea16e37bc717002d5c9
jmiano <j.miano@outlook.com>
2018-09-22 21:32:37 -0400

    fixed welcome activity layout

--- BuzzTracker/app/src/main/java/com/cs2340/binarybros/buzztracker/WelcomeActivity.java
+++ BuzzTracker/app/src/main/java/com/cs2340/binarybros/buzztracker/WelcomeActivity.java
@@ -1,7 +1,10 @@
 package com.cs2340.binarybros.buzztracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Button loginBtn = (Button) findViewById(R.id.loginButton);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
