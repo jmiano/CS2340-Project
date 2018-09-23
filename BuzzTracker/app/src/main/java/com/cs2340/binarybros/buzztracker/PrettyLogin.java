7faa93a2e2501ca5ce02dde9c5df014ed5e2bdde
jmiano <42718689+jmiano@users.noreply.github.com>
2018-09-22 22:16:39 -0400

    added cancel button functionality

--- BuzzTracker/app/src/main/java/com/cs2340/binarybros/buzztracker/PrettyLogin.java
+++ BuzzTracker/app/src/main/java/com/cs2340/binarybros/buzztracker/PrettyLogin.java
@@ -1,9 +1,11 @@
 package com.cs2340.binarybros.buzztracker;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 public class PrettyLogin extends AppCompatActivity {
@@ -26,5 +28,13 @@ public class PrettyLogin extends AppCompatActivity {
         relay1 = (RelativeLayout) findViewById(R.id.relay1);
 
         handler.postDelayed(runnable, 2000); //2000 is the timeout of the splash screen
+
+        Button cancelBtn = (Button) findViewById(R.id.cancelButton);
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PrettyLogin.this, WelcomeActivity.class));
+            }
+        });
     }
 }
