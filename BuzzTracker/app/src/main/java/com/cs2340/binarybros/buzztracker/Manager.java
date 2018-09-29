0b04f6ad0664083c5d87a37796fac13453d575f6
Walker Smith <wsmith330@gatech.edu>
2018-09-29 17:52:47 -0400

    Delete Manager.java

--- BuzzTracker/app/src/main/java/com/cs2340/binarybros/buzztracker/Manager.java
+++
@@ -1,8 +0,0 @@
-package com.cs2340.binarybros.buzztracker;
-
-public class Manager extends Admin {
-
-    public Manager() {
-        super(name, username, password, email, employeeNumber);
-    }
-}
