36932dadb97467a3c090b99690dc670ae72e8bc8
Walker Smith <wsmith330@gatech.edu>
2018-10-21 16:38:40 -0400

    Changed Intent constructor in the login method's startActivity call to point to HomeScreen

--- BuzzTracker/app/src/main/java/com/cs2340/binarybros/buzztracker/Controllers/PrettyLogin.java
+++ BuzzTracker/app/src/main/java/com/cs2340/binarybros/buzztracker/Controllers/PrettyLogin.java
@@ -73,7 +73,7 @@ public class PrettyLogin extends AppCompatActivity {
         }
 
        if (validLogin){
-           startActivity(new Intent(PrettyLogin.this, LocationManageActivity.class));
+           startActivity(new Intent(PrettyLogin.this, HomeScreen.class));
         } else {
             AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
             dlgAlert.setMessage("Please input a valid Username and Password");
