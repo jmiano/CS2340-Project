9f11b30dd25316aca9ce6179b757d0400383f55a
Fidel Flores <fefcaceres@gmail.com>
2018-10-23 21:31:53 -0400

    Implement view for adding a donation

---
+++ BuzzTracker/app/src/main/java/com/cs2340/binarybros/buzztracker/Controllers/AddDonation.java
@@ -0,0 +1,13 @@
+package com.cs2340.binarybros.buzztracker.Controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddDonation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_donation);
+    }
+}
