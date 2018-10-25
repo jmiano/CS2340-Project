db5141af577bff33ef479bbf3b8e2cdc0ea8f6f9
dannyht <dannyhtaylor@gmail.com>
2018-10-25 19:26:46 -0400

    Successfully created a filtering system for categories.

--- BuzzTracker/app/src/main/java/com/cs2340/binarybros/buzztracker/Controllers/Inventory.java
+++ BuzzTracker/app/src/main/java/com/cs2340/binarybros/buzztracker/Controllers/Inventory.java
@@ -1,5 +1,6 @@
 package com.cs2340.binarybros.buzztracker.Controllers;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
@@ -25,65 +26,146 @@ import java.util.ArrayList;
 public class Inventory extends AppCompatActivity {
 
     ArrayList<Donation> donationArrayList;
+    ArrayList<String> categoryFilterList;
+    ArrayList<Donation> finalDonationArrayList;
+    private String currentLocation;
     private Donation donation;
     private int donationItemId;
-    private TextView categoryFilterBtn;
+    private Button categoryFilterBtn;
+    private Button applyFilterBtn;
+    private Button clearFiltersBtn;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
-        ListView inventoryListView = (ListView) findViewById(R.id.inventory_list);
+
 
         /**
          * Declaring UI Elements
          */
-        categoryFilterBtn = (TextView) findViewById(R.id.category_filter);
+        categoryFilterBtn = (Button) findViewById(R.id.categoriesbtn);
+        applyFilterBtn = (Button) findViewById(R.id.applyfilterbtn);
+        clearFiltersBtn = (Button) findViewById(R.id.clearfiltersbtn);
 
         /**
          * Creating Objects for Testing
          */
-//        Donation bike = new Donation("white bicycle", "10/10/2019", "this place",
-//                "kitchen", "100.00", "this is a cool bike", "this is an even cooler bike");
-//        Donation pan = new Donation("black pan", "10/10/2011", "my house", "kitchen", "100.00",
-//                "nice pan", "");
-//        Database.getInstance().getDonationList().add(bike);
-//        Database.getInstance().getDonationList().add(pan);
+        Donation bike = new Donation("white bicycle", "", "",
+                "Kitchen", "", "", "");
+        Donation pan = new Donation("red pan", "", "", "Kitchen", "",
+                "", "");
+        Donation peanut = new Donation("brown peanut", "", "",
+                "Kitchen", "", "", "");
+        Donation person = new Donation("Fan", "", "", "Other", "",
+                "", "");
+        Database.getInstance().getDonationList().add(bike);
+        Database.getInstance().getDonationList().add(pan);
+        Database.getInstance().getDonationList().add(peanut);
+        Database.getInstance().getDonationList().add(person);
         //End creating objects
 
         donationArrayList = Database.getInstance().getDonationList();
 
-        InventoryListAdapter inventoryAdapter = new InventoryListAdapter(this, R.layout.inventory_list_adapterview, donationArrayList);
-        inventoryListView.setAdapter(inventoryAdapter);
+
 
         /**
          * Button action for choosing filtering categories
          */
-        categoryFilterBtn.setOnTouchListener(new View.OnTouchListener() {
+        categoryFilterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
+            public void onClick(View v) {
                 AlertDialog.Builder mBuilder = new AlertDialog.Builder(Inventory.this);
                 View mView = getLayoutInflater().inflate(R.layout.categoy_filter_diaglog_layout, null);
-                CheckBox categoryClothing = (CheckBox) mView.findViewById(R.id.category_filter_checkbox_clothing);
-                CheckBox categoryHat = (CheckBox) mView.findViewById(R.id.category_filter_checkbox_hat);
-                CheckBox categoryKitchen = (CheckBox) mView.findViewById(R.id.category_filter_checkbox_kitchen);
-                CheckBox categoryElectronics = (CheckBox) mView.findViewById(R.id.category_filter_checkbox_electronics);
-                CheckBox categoryHousehold = (CheckBox) mView.findViewById(R.id.category_filter_checkbox_household);
-                CheckBox categoryOther = (CheckBox) mView.findViewById(R.id.category_filter_checkbox_other);
+
+                final CheckBox categoryClothing = (CheckBox) mView.findViewById(R.id.category_filter_checkbox_clothing);
+                final CheckBox categoryHat = (CheckBox) mView.findViewById(R.id.category_filter_checkbox_hat);
+                final CheckBox categoryKitchen = (CheckBox) mView.findViewById(R.id.category_filter_checkbox_kitchen);
+                final CheckBox categoryElectronics = (CheckBox) mView.findViewById(R.id.category_filter_checkbox_electronics);
+                final CheckBox categoryHousehold = (CheckBox) mView.findViewById(R.id.category_filter_checkbox_household);
+                final CheckBox categoryOther = (CheckBox) mView.findViewById(R.id.category_filter_checkbox_other);
                 Button filterBtn = (Button) mView.findViewById(R.id.category_filter_acceptBtn);
-                Button cancelBtn = (Button) mView.findViewById(R.id.category_filter_cancelBtn);
-
-
+                Button dismissBtn = (Button) mView.findViewById(R.id.category_filter_cancelBtn);
 
                 mBuilder.setView(mView);
-                AlertDialog dialog = mBuilder.create();
+                final AlertDialog dialog = mBuilder.create();
                 dialog.show();
-                return false;
+
+                filterBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        categoryFilterList = new ArrayList<String>();
+
+                        if (categoryClothing.isChecked()) {
+                            categoryFilterList.add("Clothing");
+                        }
+
+                        if (categoryHat.isChecked()) {
+                            categoryFilterList.add("Hat");
+                        }
+
+                        if (categoryKitchen.isChecked()) {
+                            categoryFilterList.add("Kitchen");
+                        }
+
+                        if (categoryElectronics.isChecked()) {
+                            categoryFilterList.add("Electronics");
+                        }
+
+                        if (categoryHousehold.isChecked()) {
+                            categoryFilterList.add("Household");
+                        }
+
+                        if (categoryOther.isChecked()) {
+                            categoryFilterList.add("Other");
+                        }
+
+                        finalDonationArrayList = filterDonationList(donationArrayList, categoryFilterList);
+
+                        dialog.dismiss();
+                    }
+                });
+
+                dismissBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+            }});
+
+        /**
+         * Set the initial listview
+         */
+        final ListView inventoryListView = (ListView) findViewById(R.id.inventory_list);
+        final InventoryListAdapter inventoryAdapter = new InventoryListAdapter(this, R.layout.inventory_list_adapterview, donationArrayList);
+        inventoryListView.setAdapter(inventoryAdapter);
+
+        /**
+         * Button action for the refreshing the listview
+         */
+        applyFilterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finalDonationArrayList = filterDonationList(donationArrayList, categoryFilterList);
+                InventoryListAdapter newInventoryAdapter = new InventoryListAdapter(Inventory.this, R.layout.inventory_list_adapterview, finalDonationArrayList);
+                inventoryListView.setAdapter(newInventoryAdapter);
             }
         });
 
-
-
+        /**
+         * Button action for clearing all filters
+         */
+        clearFiltersBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InventoryListAdapter newInventoryAdapter = new InventoryListAdapter(Inventory.this, R.layout.inventory_list_adapterview, donationArrayList);
+                inventoryListView.setAdapter(newInventoryAdapter);
+            }
+        });
 
 
         /**
@@ -113,5 +195,29 @@ public class Inventory extends AppCompatActivity {
                 startActivity(goToAddDonationActivity);
             }
         });
+
+
     }
+
+    /**
+     * This is a filtering method that filters by location & category
+     * @param donationList blah
+     * @param categoriesSelected blah
+     * @return blah
+     */
+    private ArrayList<Donation> filterDonationList(ArrayList<Donation> donationList, ArrayList<String> categoriesSelected) {
+        if (categoriesSelected == null || categoriesSelected.size() == 0) {
+            return donationList;
+        } else {
+            ArrayList<Donation> returnDonationList = new ArrayList<>();
+            for (Donation donation: donationList) {
+                if (categoriesSelected.contains(donation.getCategory())) {
+                    returnDonationList.add(donation);
+                }
+            }
+            return returnDonationList;
+        }
+
+    }
+
 }
