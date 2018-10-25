c6cfccc248ef1411f469d2926c5dbfb5567f1c2c
dannyht <dannyhtaylor@gmail.com>
2018-10-25 09:46:27 -0400

    Pulled from Review

---
+++ BuzzTracker/app/src/main/java/com/cs2340/binarybros/buzztracker/Controllers/AddDonation.java
@@ -0,0 +1,61 @@
+package com.cs2340.binarybros.buzztracker.Controllers;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+
+import com.cs2340.binarybros.buzztracker.Models.Database;
+import com.cs2340.binarybros.buzztracker.Models.Donation;
+
+import java.util.ArrayList;
+
+public class AddDonation extends AppCompatActivity {
+    private EditText TimestampField;
+    private EditText LocationField;
+    private EditText CategoryField;
+    private EditText PriceField;
+    private EditText ShortdescriptionField;
+    private EditText LongdescriptionField;
+    private ArrayList<Donation> listdonation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_donation);
+        /**
+         * Grab the dialog widgets so we can get info for later
+         */
+        TimestampField = (EditText) findViewById(R.id.timestamp);
+        LocationField = (EditText) findViewById(R.id.location);
+        CategoryField = (EditText) findViewById(R.id.category);
+        PriceField = (EditText) findViewById(R.id.price);
+        ShortdescriptionField = (EditText) findViewById(R.id.shortDescription);
+        LongdescriptionField = (EditText) findViewById(R.id.longDescription);
+        listdonation = Database.getInstance().getDonationList();
+        Button submitBtn = (Button) findViewById(R.id.submit);
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createObject();
+                //back to home page
+            }
+        });
+    }
+    /**
+     * This method creates a donation object and puts it into database
+     *
+     */
+    private void createObject() {
+        String timestamp = TimestampField.getText().toString();
+        String location = LongdescriptionField.getText().toString();
+        String category = CategoryField.getText().toString();
+        String price = PriceField.getText().toString();
+        String shortdescription = ShortdescriptionField.getText().toString();
+        String longdescription = LongdescriptionField.getText().toString();
+        listdonation.add(new Donation(timestamp, location, category, price, shortdescription,
+                longdescription));
+    }
+}
