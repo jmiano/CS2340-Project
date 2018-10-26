8fd4d67b8552de89ff29e0a0e30e22c81d77ed83
dannyht <dannyhtaylor@gmail.com>
2018-10-25 20:43:23 -0400

    Working on filtering by locations.

--- BuzzTracker/app/src/main/java/com/cs2340/binarybros/buzztracker/Models/LocationEmployee.java
+++ BuzzTracker/app/src/main/java/com/cs2340/binarybros/buzztracker/Models/LocationEmployee.java
@@ -3,9 +3,9 @@ package com.cs2340.binarybros.buzztracker.Models;
 
 public class LocationEmployee extends User {
 
-    public LocationEmployee(String nameInput, String usernameInput, String passwordInput,
+    public LocationEmployee(String type, String nameInput, String usernameInput, String passwordInput,
                             String emailInput, String employeeLocation, int employeeID) {
-        super(nameInput, usernameInput, passwordInput, emailInput, employeeLocation, employeeID);
+        super(type, nameInput, usernameInput, passwordInput, emailInput, employeeLocation, employeeID);
     }
 
 }
