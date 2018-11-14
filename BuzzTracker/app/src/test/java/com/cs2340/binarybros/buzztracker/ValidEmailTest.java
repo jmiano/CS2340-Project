110d2f39d68175629a336ad77783e5d32b38cf36
dannyht <dannyhtaylor@gmail.com>
2018-11-14 11:11:27 -0500

    Took out 2 unecessary dependencies regarding JUnits

--- BuzzTracker/app/src/test/java/com/cs2340/binarybros/buzztracker/ValidEmailTest.java
+++ BuzzTracker/app/src/test/java/com/cs2340/binarybros/buzztracker/ValidEmailTest.java
@@ -1,10 +1,35 @@
-package com.cs2340.binarybros.buzztracker.Models;
-import com.google.common.truth.Truth.assertThat;
+package com.cs2340.binarybros.buzztracker;
+import com.cs2340.binarybros.buzztracker.Models.User;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * This class tests to see if the method that I use to test for valid emails is correct
+ */
 public class ValidEmailTest {
+    private User user;
+
+    /**
+     * Do this before every test
+     */
+    @Before
+    public void setUp() {
+        user = new User("Manager", "Daniel", "dannyhtaylor",
+                "youStink", "dtaylor87@gatech.edu",
+                "AFD Station 4", 0);
+    }
+
+    /**
+     * Actual test
+     */
     @Test
-    public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
-        assertThat(EmailValidator.isValidEmail("name@email.com")).isTrue();
+    public void emailValidator() {
+        assertFalse(user.emailIsValid(""));
+        assertFalse(user.emailIsValid("aklfhl"));
+        assertTrue(user.emailIsValid(user.getEmail()));
+
     }
 }
